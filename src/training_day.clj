bc6ac24cbf705a2682a564f7b9ac315da83170f8
johnswindin <johnswindin@gmail.com>
2015-12-29 13:17:33 +0000

    all tests pass

--- src/training_day.clj
+++ src/training_day.clj
@@ -1,11 +1,16 @@
 (ns training-day)
 
-(def answer ":(")
+(def answer 42)
 
 (def hai "O HAI!")
 
 (defn square [x]
-  ":(")
+  "returns the passed argument muliplied by itself."
+  [x]
+  (* x x))
 
 (defn average [a b]
-  ":(")
+  "returns the average of the 2 argument passed in."
+  [a b]
+  (/ (+ a b) 2))
+
